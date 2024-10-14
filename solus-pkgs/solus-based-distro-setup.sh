047244e134cefd974393c3379511aa36459d45b2
Greekie via Linux <greekie.via.linux@gmail.com>
2024-10-14 03:15:47 -0400

    Add files via upload

---
+++ solus-pkgs/solus-based-distro-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd &&
+sudo eopkg install neofetch fastfetch git nano flatpak &&
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+cd &&
+cd Downloads &&
+sudo flatpak install dev.vencord.Vesktop.flatpakref
+
+clear &&
+exit
