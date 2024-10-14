5c6cf7db53fafea9a649b566865d2bb4811490e3
Greekie via Linux <greekie.via.linux@gmail.com>
2024-10-14 19:32:07 -0400

    Add files via upload

---
+++ debian-pkgs/debian-based-distro-setup-gnome.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd &&
+sudo apt install neofetch fastfetch git nano flatpak gnome-software-plugin-flatpak &&
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+cd &&
+cd Downloads &&
+sudo dpkg -i virtualbox-7.1_7.1.2-164945~Debian~bookworm_amd64.deb
+
+cd &&
+cd Downloads &&
+sudo flatpak install dev.vencord.Vesktop.flatpakref
+
+clear &&
+exit
